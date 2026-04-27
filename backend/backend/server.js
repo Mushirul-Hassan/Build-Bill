c1fa0577345bda768248e0064a8710e1aea54960
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-04-27 19:23:12 +0530

    Added localhost url in CORS

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -1,35 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import express from "express";
 import mongoose from "mongoose";
 
 import cors from "cors";
-import InvoiceRoutes from "./routes/InvoiceRoutes.js"
+import InvoiceRoutes from "./routes/InvoiceRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
-import adminRoutes from './routes/adminRoutes.js';
+import adminRoutes from "./routes/adminRoutes.js";
 // import userRoutes from './routes/userRoutes.js';
-import reportRoutes from './routes/reportRoutes.js'; 
-import clientRoutes from './routes/clientRoutes.js';
-import emailRoutes from './routes/emailRoutes.js';
-import pdfRoutes from './routes/pdfRoutes.js';
-
-
-
-
+import reportRoutes from "./routes/reportRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import emailRoutes from "./routes/emailRoutes.js";
+import pdfRoutes from "./routes/pdfRoutes.js";
 
 const app = express();
-app.use(cors({origin: 'https://build-bill.vercel.app'}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://build-bill.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/invoices", InvoiceRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use('/api/reports', reportRoutes);
-app.use('/api/clients', clientRoutes);
-app.use('/api/email', emailRoutes);
-app.use('/api/pdf', pdfRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/email", emailRoutes);
+app.use("/api/pdf", pdfRoutes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -45,6 +45,3 @@ mongoose
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
-
-
-  
